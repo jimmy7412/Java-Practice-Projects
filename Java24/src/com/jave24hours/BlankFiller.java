755b970e179260f4397b48ca66a9cd20a3d99e16
Unknown <jimosito@gmail.com>
2018-08-23 15:36:50 -0700

    Added 2 new practice programs

---
+++ Java24/src/com/jave24hours/BlankFiller.java
@@ -0,0 +1,17 @@
+package com.jave24hours;
+
+public class BlankFiller {
+
+	public static void main(String[] arguments) {
+		// Fill in the Blanks
+		System.out.println("the " + arguments[0]
+		+ " "
+		+ arguments[1]
+		+ " fox "
+		+ "jumped over the "
+		+ arguments[2]
+		+ " dog."
+		);
+	}
+
+}
---
+++ Java24/src/com/jave24hours/Root.java
@@ -0,0 +1,16 @@
+package com.jave24hours;
+
+public class Root {
+
+	public static void main(String[] args) {
+		//An app to find the square root of a number
+		int number = 225;
+		System.out.println("The square root of "
+				+ number
+				+ " is "
+				+ Math.sqrt(number)
+				);
+
+	}
+
+}
